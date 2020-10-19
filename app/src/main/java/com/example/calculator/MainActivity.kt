24e39ef7056d728699cb8ed1992078ea3dbb056e
Phone-game <darylvdberg@gmail.com>
2020-10-19 16:38:44 +0200

    Added a clear button, zero button and dot button

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -24,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         val divideButton = findViewById<Button>(R.id.divide) as Button
         val timesButton = findViewById<Button>(R.id.times) as Button
         val calcButton = findViewById<Button>(R.id.calc) as Button;
+        val dotbutton = findViewById<Button>(R.id.dot) as Button;
+        val clearButton = findViewById<Button>(R.id.clear) as Button;
 
         calculation = Calculation();
 
@@ -34,14 +35,19 @@ class MainActivity : AppCompatActivity() {
         minusButton.setOnClickListener(printButtonToScreen);
         divideButton.setOnClickListener(printButtonToScreen);
         timesButton.setOnClickListener(printButtonToScreen);
+        dotbutton.setOnClickListener(printButtonToScreen);
+
+        clearButton.setOnClickListener(View.OnClickListener { _ ->
+            screen.text = "";
+        })
 
         screen = findViewById(R.id.screen);
         getCalculatorButtons();
     }
 
 
-    fun getCalculatorButtons(){
-        for(i in 1..9){
+    private fun getCalculatorButtons(){
+        for(i in 0..9){
             val buttonId = "button"+i;
             val viewId = resources.getIdentifier(buttonId, "id", packageName);
             val button = findViewById<Button>(viewId) as Button;
@@ -50,20 +56,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun updateScreen(text : String){
+    private fun updateScreen(text : String){
         screen.append(text);
     }
 
-    val printButtonToScreen = View.OnClickListener{view ->
+    private val printButtonToScreen = View.OnClickListener{ view ->
         val button = view.findViewById<Button>(view.id);
         updateScreen(button.text.toString());
     }
 
-    val removeNumberFromScreen = View.OnClickListener { view ->
+    private val removeNumberFromScreen = View.OnClickListener { _ ->
         screen.text = screen.text.dropLast(1);
     }
 
-    val calculateNumbersOnScreen = View.OnClickListener { view ->
+    private val calculateNumbersOnScreen = View.OnClickListener { _ ->
         val reg = Regex("(?<=[+/*-])|(?=[+/*-])")
         val values = screen.text.split(reg);
         val a = values[0].toString().toDouble();
